4fb0c4dac063c464c5e46006cf5c9e77353a3845
anower00 <anower.hasan@gmail.com>
2019-03-05 17:41:07 +0600

    First commit

---
+++ resources/js/components/Conversation.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="conversation">
+        <h1>{{ contact ? contact.name : 'select a contact'}}</h1>
+         <MessagesFeed :contact="contact" :messages="messages"/>
+         <MessageComposer @send="sendMessage"/>
+    </div>
+</template>
+
+<script>
+    import MessagesFeed from './MessagesFeed';
+    import MessageComposer from './MessageComposer';
+
+export default {
+    props:{
+        contact:{
+            type: Object,
+            default:null
+        },
+        messages:{
+            type: Array,
+            default: []
+        }
+    },
+    methods:{
+        sendMessage(text){
+            console.log(text);
+        }
+    }
+    
+}
+</script>
