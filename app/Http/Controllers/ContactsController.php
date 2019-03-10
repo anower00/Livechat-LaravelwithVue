ab4e8e6ab276d29d8b8320ae04e40edac65d9fa2
anower00 <anower.hasan@gmail.com>
2019-03-10 18:06:16 +0600

    only auth user can write and see messages

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -20,6 +20,14 @@ class ContactsController extends Controller
     {
         $messages = Message::where('from',$id)->orWhere('to',$id)->get();
 
+        $messages = Message::where(function($q) use ($id){
+            $q->where('from',auth()->id());
+            $q->where('to',$id);
+        })->orWhere(function($q) use ($id) {
+            $q->where('from', $id);
+            $q->where('to', auth()->id());
+        })->get();
+
         return response()->json($messages);
     }
 
